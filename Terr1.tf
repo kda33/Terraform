df43e42b46574549798cac250f972c4c41accd36
KDA33 <SSAAZ@pm.me>
2021-02-04 14:36:02 +0500

    v.1

---
+++ Terr1.tf
@@ -0,0 +1,41 @@
+#----------------------------------------------------------
+#--------- Build Web Server during Bootstrap
+#----------------------------------------------------------
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0a6dc7529cd559185"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+  user_data              = file("user_data1.sh")
+  key_name="Frankfurt-key"
+
+  tags = {Name = "Web Server Build by Terraform"}
+}
+
+resource "aws_security_group" "web_server" {
+  name        = "WebServer Security Group"
+  description = "SecurityGroup"
+
+  dynamic "ingress" {
+    for_each      = ["80", "443","22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {Name = "Web Server SecurityGroup"}
+}
---
+++ user_data1.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+echo "-----------------------------------S-T-A-R-T------------------------------------"
+sudo yum update -y
+sudo yum install git -y
+sudo yum install -y httpd
+echo '<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="KDA33">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Starter Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/starter-template/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Brand</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item active">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown01">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<main class="container">
+
+  <div class="starter-template text-center py-5 px-3">
+    <br>
+
+    <h1>Build by AWS, Terraform, Apache, Bootstrap</h1>
+    <p class="lead">Use this as a way to quickly start any new project.<br>Congratulations! you did it. </p>
+
+    <img src="https://seeklogo.com/images/A/amazon-web-services-aws-logo-6C2E3DCD3E-seeklogo.com.png" alt="..." class="img-rounded">
+    <img src="https://avatars.githubusercontent.com/u/29154831?s=200&v=4" alt="..." class="img-rounded">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIfRAjJzTm-o2pY2dDQiEKjYj_1vOB7NXUA&usqp=CAU" alt="..." class="img-rounded">
+    <img src="https://avatars.githubusercontent.com/u/2918581?s=200&v=4" alt="..." class="img-rounded">
+  </div>
+
+</main><!-- /.container -->
+
+
+    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>' > /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "-----------------------------------F-I-N-I-S-H----------------------------------"
